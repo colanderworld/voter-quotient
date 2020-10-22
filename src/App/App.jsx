434b4248150b354c4c0d2695e6f58fea3c9d0fe4
Eli Cohen <eliunited@gmail.com>
2020-10-22 12:25:51 -0500

    Main

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,38 +1,38 @@
 // Library imports
-import React from "react";
-import { Router } from "@reach/router";
-import { Grommet, Box } from "grommet";
+import React from 'react'
+import { Router } from '@reach/router'
+import { Grommet, Box, Main } from 'grommet'
 
 // Informational pages
-import Home from "../Pages/Home";
-import About from "../Pages/About";
-import Questions from "../Pages/Questions";
-import Contact from "../Pages/Contact";
+import Home from '../Pages/Home'
+import About from '../Pages/About'
+import Questions from '../Pages/Questions'
+import Contact from '../Pages/Contact'
 
 // Component imports
-import Footer from "../Components/Footer";
+import Footer from '../Components/Footer'
 
 // Grommet global theme
-import Theme from "../Utils/Theme.json";
+import Theme from '../Utils/Theme.json'
 
 export default () => {
-  return (
-    <Grommet theme={Theme} full>
-      <Box fill="vertical">
-        <Box flex="grow" margin={{ bottom: "10em" }}>
-          <Box align="center">
-            <Box fill align="left" width={{ max: "800px" }}>
-              <Router>
-                <Home path="/" />
-                <About path="about" />
-                <Questions path="questions" />
-                <Contact path="contact" />
-              </Router>
-            </Box>
-          </Box>
-        </Box>
-        <Footer />
-      </Box>
-    </Grommet>
-  );
-};
+	return (
+		<Grommet theme={Theme} full>
+			<Main>
+				<Box flex='grow' margin={{ bottom: '10em' }}>
+					<Box align='center'>
+						<Box fill align='left' width={{ max: '800px' }}>
+							<Router>
+								<Home path='/' />
+								<About path='about' />
+								<Questions path='questions' />
+								<Contact path='contact' />
+							</Router>
+						</Box>
+					</Box>
+				</Box>
+				<Footer />
+			</Main>
+		</Grommet>
+	)
+}
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,119 +1,115 @@
-import React, { useState, useContext } from "react";
-import styled from "styled-components";
-import axios from "axios";
-import usePlacesAutocomplete from "use-places-autocomplete";
-import { Box, Text, ResponsiveContext } from "grommet";
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+import usePlacesAutocomplete from 'use-places-autocomplete'
+import { Box, Text } from 'grommet'
 import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption
-} from "@reach/combobox";
-import "@reach/combobox/styles.css";
-import Lookup from "./Lookup";
-import { Wave } from "better-react-spinkit";
+	Combobox,
+	ComboboxInput,
+	ComboboxPopover,
+	ComboboxList,
+	ComboboxOption
+} from '@reach/combobox'
+import '@reach/combobox/styles.css'
+import Lookup from './Lookup'
+import { Wave } from 'better-react-spinkit'
 
 export default () => {
-  const size = useContext(ResponsiveContext);
+	const [positions, setPositions] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
+	const [isLoaded, setIsLoaded] = useState(false)
 
-  const [positions, setPositions] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isLoaded, setIsLoaded] = useState(false);
+	const {
+		ready,
+		value,
+		suggestions: { status, data },
+		setValue
+	} = usePlacesAutocomplete({
+		requestOptions: {
+			componentRestrictions: { country: 'us' }
+		},
+		debounce: 200
+	})
 
-  const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue
-  } = usePlacesAutocomplete({
-    requestOptions: {
-      componentRestrictions: { country: "us" }
-    },
-    debounce: 200
-  });
+	const handleInput = e => {
+		setValue(e.target.value)
+	}
 
-  const handleInput = e => {
-    setValue(e.target.value);
-  };
+	const handleSelect = val => {
+		setValue(val, false)
+		setIsLoading(true)
+		axios(`https://beta-dwv-api.vercel.app/api?address=${encodeURI(val)}`).then(res => {
+			console.log(res)
+			setPositions(res.data.data.filter(({ tagged }) => tagged))
+			setIsLoading(false)
+			setIsLoaded(true)
+		})
+	}
 
-  const handleSelect = val => {
-    setValue(val, false);
-    setIsLoading(true);
-    axios(`https://beta-dwv-api.vercel.app/api?address=${encodeURI(val)}`).then(
-      res => {
-        console.log(res);
-        setPositions(res.data.data.filter(({ tagged }) => tagged));
-        setIsLoading(false);
-        setIsLoaded(true);
-      }
-    );
-  };
-
-  return (
-    <>
-      <Combo onSelect={handleSelect} aria-labelledby="demo">
-        <Box fill={true} width={{ max: "650px" }}>
-          <Input
-            value={value}
-            onChange={handleInput}
-            disabled={!ready}
-            width="650px"
-            placeholder="Enter your address to begin 🗳"
-          />
-          <Popover>
-            <ComboboxList>
-              {status === "OK" ? (
-                data.map(({ id, description }) => (
-                  <ComboboxOption key={id} value={description} />
-                ))
-              ) : (
-                <div>
-                  <span aria-label="embaressed face emoji" role="img">
-                    😳
-                  </span>
-                  &nbsp;We couldn't find that address! Please try another.
-                </div>
-              )}
-            </ComboboxList>
-          </Popover>
-        </Box>
-      </Combo>
-      {isLoading ? (
-        <Box align="center" gap="medium">
-          <Text size="16px" weight="bold">
-            Getting your election data! Please wait.
-          </Text>
-          <Wave size={50} />
-        </Box>
-      ) : (
-        isLoaded && <Lookup data={positions}></Lookup>
-      )}
-    </>
-  );
-};
+	return (
+		<>
+			<Combo onSelect={handleSelect} aria-labelledby='demo'>
+				<Box fill={true} width={{ max: '650px' }}>
+					<Input
+						value={value}
+						onChange={handleInput}
+						disabled={!ready}
+						width='650px'
+						placeholder='Enter your address to begin 🗳'
+					/>
+					<Popover>
+						<ComboboxList>
+							{status === 'OK' ? (
+								data.map(({ id, description }) => (
+									<ComboboxOption key={id} value={description} />
+								))
+							) : (
+								<div>
+									<span aria-label='embaressed face emoji' role='img'>
+										😳
+									</span>
+									&nbsp;We couldn't find that address! Please try another.
+								</div>
+							)}
+						</ComboboxList>
+					</Popover>
+				</Box>
+			</Combo>
+			{isLoading ? (
+				<Box align='center' gap='medium'>
+					<Text size='16px' weight='bold'>
+						Getting your election data! Please wait.
+					</Text>
+					<Wave size={50} />
+				</Box>
+			) : (
+				isLoaded && <Lookup data={positions}></Lookup>
+			)}
+		</>
+	)
+}
 
 const Combo = styled(Combobox)`
-  margin: 0 0 4em 0;
-`;
+	margin: 0 0 4em 0;
+`
 
 const Popover = styled(ComboboxPopover)`
-  padding: 2px;
-  font-size: 1.5em;
-  line-height: 1.2em;
-  max-width: 594px;
-  font-family: sans-serif;
-`;
+	padding: 2px;
+	font-size: 1.5em;
+	line-height: 1.2em;
+	max-width: 594px;
+	font-family: sans-serif;
+`
 
 const Input = styled(ComboboxInput)`
-  margin-top: 2em;
-  font-size: 2em;
-  border: 0;
-  outline: 0;
-  background: transparent;
-  border-bottom: 1px solid black;
+	margin-top: 2em;
+	font-size: 2em;
+	border: 0;
+	outline: 0;
+	background: transparent;
+	border-bottom: 1px solid black;
 
-  :focus {
-    outline: none;
-  }
-`;
+	:focus {
+		outline: none;
+	}
+`
