95cff809c7f87ef09ca7d919c8f07d69068a2472
Eli Cohen <eliunited@gmail.com>
2020-10-22 12:33:14 -0500

    Justify center

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,31 +1,32 @@
 // Library imports
-import React, { useContext } from "react";
-import styled from "styled-components";
-import { ResponsiveContext, Text, Heading, Box } from "grommet";
-import Input from "../Components/Input";
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import { ResponsiveContext, Text, Heading, Box } from 'grommet'
+import Input from '../Components/Input'
 
 const Home = () => {
-  const size = useContext(ResponsiveContext);
-  return (
-    <>
-      <Box margin={size === "small" && { horizontal: "medium" }}>
-        <Title>Don't Wait, Vote!</Title>
-        <Text>
-          This summer has proved that policing in America is broken.
-          {size !== "small" ? <br /> : <span>&nbsp;</span>}
-          Politicians in Washington D.C. won't fix the problem.
-          {size !== "small" ? <br /> : <span>&nbsp;</span>}
-          Learn about the <b>elected offices</b> that can <b>actually</b> make a
-          difference.
-        </Text>
-      </Box>
-      <Input />
-    </>
-  );
-};
+	const size = useContext(ResponsiveContext)
+	return (
+		<>
+			<Box margin={size === 'small' && { horizontal: 'medium' }}>
+				<Title>Don't Wait, Vote!</Title>
+				<Text>
+					This summer has proved that policing in America is broken.
+					{size !== 'small' ? <br /> : <span>&nbsp;</span>}
+					Politicians in Washington D.C. won't fix the problem.
+					{size !== 'small' ? <br /> : <span>&nbsp;</span>}
+					Learn about the <b>elected offices</b> that can <b>actually</b> make a difference.
+				</Text>
+			</Box>
+			<Box justify='center'>
+				<Input />
+			</Box>
+		</>
+	)
+}
 
 const Title = styled(Heading)`
-  font-family: "IBM Plex Mono", monospace;
-`;
+	font-family: 'IBM Plex Mono', monospace;
+`
 
-export default Home;
+export default Home
