f49c8e4708aa8e861d58de39bc715bf864cbf29e
Eli Cohen <eliunited@gmail.com>
2020-10-26 16:18:26 -0500

    keep it goin

--- src/Components/Position.jsx
+++ src/Components/Position.jsx
@@ -1,6 +1,7 @@
 // LIbrary imports
-import React, { useContext } from 'react'
-import { AccordionPanel, Box, Text, ResponsiveContext, Anchor, Heading } from 'grommet'
+import React, { useContext, useRef, useState } from 'react'
+import { AccordionPanel, Box, Text, ResponsiveContext, Anchor, Heading, Drop } from 'grommet'
+import { Star } from 'grommet-icons'
 import _ from 'lodash'
 
 // Component imports
@@ -8,9 +9,11 @@ import Candidate from './Candidate'
 import MetaInfo from './MetaInfo'
 
 export default ({ data }) => {
-	const { normalized_position_name, position_name, description, level, candidates } = data
+	const { normalized_position_name, position_name, description, level, candidates, starred } = data
 
 	const size = useContext(ResponsiveContext)
+	const [over, setOver] = useState()
+	const ref = useRef()
 
 	const uniqueCandidates = _.uniqBy(candidates, 'name')
 	console.log(uniqueCandidates)
@@ -42,8 +45,33 @@ export default ({ data }) => {
 						<Text weight='bold' style={{ fontSize: '1.6em', lineHeight: '1.5em' }}>
 							{position_name}
 						</Text>
+						{starred && (
+							<Box
+								ref={ref}
+								onMouseOver={() => size !== 'small' && setOver(true)}
+								onMouseOut={() => size !== 'small' && setOver(false)}>
+								<Star color='gold' size='30px' />
+							</Box>
+						)}
 					</Box>
-
+					{ref.current && over && (
+						<Drop align={{ bottom: 'top' }} target={ref.current} plain>
+							<Box
+								width={starred ? '250px' : '200px'}
+								pad='xsmall'
+								background='white'
+								margin={{ bottom: '4px' }}
+								border={{ size: 'xsmall', color: 'black' }}
+								round={{ size: 'xsmall' }}>
+								<Text size='small' style={{ display: !starred && 'none' }}>
+									This is one of our <b>highlighted races</b>.
+								</Text>
+								<Text size='small'>
+									This position has a <b>direct impact on criminal justice</b>.
+								</Text>
+							</Box>
+						</Drop>
+					)}
 					<Box direction='row' alignSelf='start' align='baseline'>
 						<MetaInfo
 							normalizedPosition={normalized_position_name}
@@ -71,30 +99,10 @@ export default ({ data }) => {
 				) : (
 					<>
 						<Box basis={size === 'small' ? 'full' : '1/2'} direction='column'>
-							{Democrat.length === 0 ? (
-								<Box
-									border={{ color: 'brand', size: 'xsmall' }}
-									pad='medium'
-									margin='medium'
-									round='medium'>
-									<Heading level={2}>Running unopposed</Heading>{' '}
-								</Box>
-							) : (
-								Democrat
-							)}
+							{Democrat}
 						</Box>
 						<Box basis={size === 'small' ? 'full' : '1/2'} direction='column'>
-							{Other.length === 0 ? (
-								<Box
-									border={{ color: 'brand', size: 'xsmall' }}
-									pad='medium'
-									margin='medium'
-									round='medium'>
-									<Heading level={2}>Running unopposed</Heading>{' '}
-								</Box>
-							) : (
-								Other
-							)}
+							{Other}
 						</Box>
 					</>
 				)}
