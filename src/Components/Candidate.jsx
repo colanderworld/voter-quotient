59f0e96115b0a8e4a9501239db5ea33f483ff61d
Eli Cohen <eliunited@gmail.com>
2020-10-27 12:46:57 -0500

    key endorsements

--- src/Components/Candidate.jsx
+++ src/Components/Candidate.jsx
@@ -1,8 +1,32 @@
 import React, { useState } from 'react'
 import { Box, Avatar, Text, Anchor } from 'grommet'
-import { User } from 'grommet-icons'
+import { Underline, User } from 'grommet-icons'
 import { getPartyColor } from '../Utils/Helpers'
 import _ from 'lodash'
+import { black } from 'grommet-controls/components/basicColors'
+
+const KEY_ENDORSEMENTS = [
+	'Working Families Party',
+	'Run for Something',
+	'Brady United Against Gun Violence',
+	'Democracy for America',
+	'Everytown for Gun Safety',
+	'Warren Democrats',
+	'New American Leaders Action Fund',
+	"People for the American Way's Next Up Victory Fund",
+	'Indivisible',
+	'Progressive Change Campaign Committee',
+	'CollectivePac.org',
+	'Latino Victory Fund',
+	'MoveOn.org',
+	'Asian American Action Fund',
+	'Justice Democrats',
+	'Giffords Courage to Fight Gun Violence',
+	'Center for Popular Democracy',
+	'Democratic Socialists of America',
+	'Emgage Action',
+	'Color of Change'
+]
 
 export default ({ data }) => {
 	const {
@@ -37,33 +61,51 @@ export default ({ data }) => {
 				</Box>
 			</Box>
 			<Box pad={{ bottom: 'medium' }} gap='xsmall'>
-				<Text size='16px' weight='bold'>
-					Endorsements
-				</Text>
-				{_.sortBy(endorsements, 'name')
-					.map(({ name, description, website_url }) => (
-						<Box>
-							<Text size='small'>{name}</Text>
-						</Box>
-					))
-					.slice(0, 5)}
-				{endorsements.length > 5 && (
+				{endorsements.filter(({ name }) => KEY_ENDORSEMENTS.includes(name)).length !== 0 ? (
 					<>
-						<Anchor
-							weight='bold'
-							onClick={() => {
-								expand ? setExpand(false) : setExpand(true)
-							}}
-							label='Show more'
-						/>
-						{expand &&
-							_.sortBy(endorsements, 'name')
-								.map(({ name, description, website_url }) => (
-									<Box>
-										<Text size='small'>{name}</Text>
-									</Box>
-								))
-								.slice(5)}
+						<Text size='16px' weight='bold'>
+							Key Endorsements
+						</Text>{' '}
+						{_.sortBy(endorsements, 'name')
+							.filter(({ name }) => KEY_ENDORSEMENTS.includes(name))
+							.map(({ name, description, website_url }) => (
+								<Box>
+									<Text size='small'>{name}</Text>
+								</Box>
+							))}
+						{endorsements.filter(({ name }) => KEY_ENDORSEMENTS.includes(name)).length !==
+							0 && (
+							<>
+								<Anchor
+									margin={{ top: 'small' }}
+									weight='bold'
+									style={{ textDecoration: 'underline', color: 'black' }}
+									onClick={() => {
+										expand ? setExpand(false) : setExpand(true)
+									}}
+									label='Show All'
+								/>
+								{expand &&
+									_.sortBy(endorsements, 'name')
+										.filter(({ name }) => !KEY_ENDORSEMENTS.includes(name))
+										.map(({ name, description, website_url }) => (
+											<Box>
+												<Text size='small'>{name}</Text>
+											</Box>
+										))}
+							</>
+						)}
+					</>
+				) : (
+					<>
+						<Text size='16px' weight='bold'>
+							Endorsements
+						</Text>{' '}
+						{_.sortBy(endorsements, 'name').map(({ name, description, website_url }) => (
+							<Box>
+								<Text size='small'>{name}</Text>
+							</Box>
+						))}
 					</>
 				)}
 				<Text size='16px' weight='bold' margin={{ top: 'medium' }}>
